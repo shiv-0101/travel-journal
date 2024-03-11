0d5bd9cfca4c2c594038a8dd7b874fd513c9c603
Shivendra Thakur <shivendrathakur321@outlook.com>
2024-03-11 17:01:05 +0530

    designed the page

--- src/components/content.jsx
+++ src/components/content.jsx
@@ -1,4 +1,5 @@
 import '../App.css'
+import location from '../assets/location.png'
 import data from '../data.js'
 
 function Content(props){
@@ -9,12 +10,14 @@ function Content(props){
             </div>
             <div className="right">
                 <div className="sec0">
+                    <img src={location} alt="" />
                     <p className='location'>{props.item.location}</p>
                     <a href={props.item.googleMapsUrl} className='google-map-link'>View on Google Maps</a>
                 </div>
                 <h1 className='title'>{props.item.title}</h1>
                 <h4 className='date'>{props.item.startDate} - {props.item.endDate}</h4>
                 <p className='description'>{props.item.description}</p>
+                <a href="https://source.unsplash.com/WLxQvbMyfas"><button className='readmore-btn'>Read More</button></a>
             </div>
         </div>
     )
@@ -37,7 +40,6 @@ const section = data.map(item => {
 function mainarea(){
     return(
         <div className='mainarea'>
-
          {section}
         </div>
     )
